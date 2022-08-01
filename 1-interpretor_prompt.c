1a99135eca9e3f3ff343b2b236b79c9816c9955f
OttoNyongole <nyongole.otto@gmail.com>
2022-08-01 14:37:56 +0300

    header file modification

--- 1-interpretor_prompt.c
+++ 1-interpretor_prompt.c
@@ -4,11 +4,10 @@
  * prompt_user - print $ to take  user inputs
  * Return: no return
  */
-
 void prompt_user(void)
 {
 	if ((isatty(STDIN_FILENO) == 1) && (isatty(STDOUT_FILENO) == 1))
 		flags.interactive = 1;
 	if (flags.interactive)
-		write(STDERR_FILENO, "$", 2);
+		write(STDERR_FILENO, "$ ", 2);
 }
