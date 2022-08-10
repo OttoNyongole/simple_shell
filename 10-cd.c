3b837a20892f126324ec017970c76079805ffa00
lumanya <shaibulumanya@gmail.com>
2022-08-10 23:10:22 +0300

    change directory

--- 10-cd.c
+++ 10-cd.c
@@ -1,107 +1,101 @@
-include "main.h"
+#include "shell.h"
 /**
- * _cd - function cd
- * desc: change between directories
- * @argv: the parameters
- * @e : the executions counter
- * @lenv: the enviroment list
- * Return: EXIT_SUCCESS in success.
+ *_cd - change directory builtin
+ * @av: argument to work with
+ * Return: int
  */
-int _cd(char **argv, lenv_s **lenv, unsigned int *e)
+int _cd(char *av[])
 {
-	char ynprn = 0, *home = NULL, *pwd = NULL, *prev = NULL, *new = NULL, *av[4];
-	unsigned int size = 512;
+	char *oldpwd = NULL, *newpath, *pathbit, *newptr;
+	char *homestr = "HOME", *oldpwdstr = "OLDPWD";
+	int ret, printpath = 0;
 
-	home = _getenv("HOME", lenv);			/* Get home from environment */
-	pwd = _getenv("OLDPWD", lenv);			/* Get oldpwd from environment */
-	if (askcwd(&prev, size) == -1)			/* Get previous path, if fail resize */
+	oldpwd = getcwd(oldpwd, 0);
+	if (oldpwd == NULL)
+		return (1);
+	if (av[1] == NULL || av[1][0] == 0)
 	{
-		free(home), free(pwd);
+		newpath = _getenv(homestr);
+		if (newpath == homestr)
+			newpath = _strdup("");
+		av[1] = newpath;
+		av[2] = NULL;
+	}
+	else if (av[1][0] == '-' && av[1][1] == 0)
+	{
+		/*check getenv malloc error here and above*/
+		newpath = _getenv(oldpwdstr);
+		if (newpath == oldpwdstr)
+			newpath = _strdup("");
+		printpath = 1;
+		free(av[1]);
+		av[1] = newpath;
+	}
+	/*{*/
+#ifdef DEBUGCD
+	printf("Making new path %s:%c\n", av[1], av[1][0]);
+#endif
+	newpath = malloc(sizeof(char) * (_strlen(oldpwd) + _strlen(av[1]) + 2));
+	if (newpath == NULL)
 		return (-1);
-	}
-	if (argv[2] == NULL && home != NULL)	/* When "cd" (only) */
+	newptr = newpath;
+	pathbit = oldpwd;
+	if (av[1][0] != '/' && pathbit[1] != 0)
+		while (*pathbit)
+			*newptr++ = *pathbit++;
+	*newptr++ = '/';
+	pathbit = strtok(av[1], "/");
+#ifdef DEBUGCD
+	printf("starting newpath:%s:Pathbit got:%s\n", newpath, pathbit);
+	printf("newpath/ptr diff:%p\n", newptr - newpath);
+#endif
+	while (pathbit != NULL)
 	{
-		if (chdir(home) == -1)
+		if (pathbit[0] == '.' && pathbit[1] == '.'
+		    && pathbit[2] == 0)
 		{
-			prnerr(argv[0], e, home), free(home), free(pwd), free(prev);
-			return (-1);
+#ifdef DEBUGCD
+			printf("going back a directory%s:%s\n", newpath, newpath);
+#endif
+			newptr--;
+			if (newptr != newpath)
+				newptr--;
+			while (*newptr != '/')
+				newptr--;
+			*newptr++ = '/';
 		}
-	}
-	else if (argv[2][0] == '~')				/* When "cd ~" */
-	{
-		if (chdir(home) == -1)
+		else if (!(pathbit[0] == '.' && pathbit[1] == 0))
 		{
-			prnerr(argv[0], e, home), free(home), free(pwd), free(prev);
-			return (-1);
+			while (*pathbit)
+				*newptr++ = *pathbit++;
+			*newptr++ = '/';
 		}
+		pathbit = strtok(NULL, "/");
+#ifdef DEBUGCD
+		printf("Got pathbit:%s\n", pathbit);
+#endif
 	}
-	else if (argv[2][0] == '-')				/* When "cd -" */
+	if (newptr != newpath && newptr != newpath + 1)
+		newptr--;
+	*newptr = 0;
+#ifdef DEBUGCD
+	printf("New path:%s\n", newpath);
+#endif
+	/*}*/
+	ret = chdir(newpath);
+	if (ret == 0)
 	{
-		if (chdir(pwd) == -1)
-		{
-			prnerr(argv[0], e, pwd), free(home), free(pwd), free(prev);
-			return (-1);
-		}
-		ynprn = 1;
+		_setenv("OLDPWD", oldpwd);
+		free(oldpwd);
+		_setenv("PWD", newpath);
+		if (printpath)
+			fprintstrs(1, newpath, "\n", NULL);
+		free(newpath);
+		return (0);
 	}
-	else
-	{
-		if (chdir(argv[2]) == -1)			/* When "cd path" */
-		{
-			prnerr(argv[0], e, argv[2]), free(home), free(pwd), free(prev);
-			return (-1);
-		}
-	}
-	if (askcwd(&new, size) == -1)			/* Get new path, if fail resize */
-	{
-		free(home), free(pwd), free(prev);
-		return (-1);
-	}
-	av[0] = "./hsh", av[1] = "cd", av[2] = "OLDPWD", av[3] = prev;
-	_setenv(av, lenv, e);				/* Set the new path in environment */
-	av[0] = "./hsh", av[1] = "cd", av[2] = "PWD", av[3] = new;
-	_setenv(av, lenv, e);
-	if (ynprn)								/* Print the new path when necessary */
-		write(STDOUT_FILENO, new, _strlen(new)), write(STDOUT_FILENO, "\n", 1);
-	free(prev), free(new);
-
-	return (EXIT_SUCCESS);
-}
-/**
- * askcwd - Get the current path
- * @path: the variable to store de value
- * @size: the lenght of bytes for the variable
- * Return: EXIT_SUCCESS when success, -1 if fails.
- */
-int askcwd(char **path, unsigned int size)
-{
-	if (*path != NULL)
-		free(path);
-	*path = malloc(size);
-	if (*path == NULL)
-		return (-1);
-	while (getcwd(*path, size) == NULL)		/* If fail to store */
-	{
-		size += 64;							/* Expands the size of buff */
-		free(path);
-		*path = malloc(size);				/* Try to get the memory from heap */
-		if (*path == NULL)
-			return (-1);
-	}
-	return (EXIT_SUCCESS);
-}
-
-/**
- * prnerr - Prints the errors on stdout
- * @prg: the name of shell
- * @e: the execution counter
- * @path: the path that fails
- * Return: Nothing.
- */
-void prnerr(char *prg, unsigned int *e, char *path)
-{
-char msg[80];
-
-	sprintf(msg, "%s: %d: cd: can't cd to %s\n", prg, *e, path);
-	write(STDERR_FILENO, msg, _strlen(msg));
+	printerr(": cd: can't cd to ");
+	fprintstrs(STDERR_FILENO, av[1], "\n", NULL);
+	free(oldpwd);
+	free(newpath);
+	return (ret);
 }
