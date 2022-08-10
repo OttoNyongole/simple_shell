9f6e3d63c375ba7e5e20ab96d5b19e65131e53ba
lumanya <shaibulumanya@gmail.com>
2022-08-10 21:08:13 +0300

    check checker

--- 0-betty_checks.c
+++ 0-betty_checks.c
@@ -1,14 +1,20 @@
 #include "main.h"
+
 /**
  * checker - checks wheather its a built in function.
  * @cmd: tokenized user input.
  * @buf: line drived frm get line function
+ *
  * Return: 1 if cmd executed 0 if not.
  */
+
 int checker(char **cmd, char *buf)
 {
+
 	if (handle_builtin(cmd, buf))
+	{
 		return (1);
+	}
 	else if (**cmd == '/')
 	{
 		execution(cmd[0], cmd);
